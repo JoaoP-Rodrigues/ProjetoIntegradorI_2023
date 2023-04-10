9c8ca1a609c479cbe31f457d4f0e151d2ed18912
Joao Paulo Rodrigues <86681094+JoaoP-Rodrigues@users.noreply.github.com>
2023-04-10 18:02:14 -0300

    Deletado informações

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from databases import DataBase
 
 obj = DataBase()
 obj.create_connection()
-post = ('98765432102', 'Joao Paulo', '13272350', 'Rua Joao do Ze, 350', '19988997766', '2102210@aluno.univesp.br')
+post = ('98765432102', 'Joao Paulo', '13272350', 'Rua Joao do Ze, 350', '19988997766', '9999999@aluno.univesp.br')
 tabela = 'INSCRICOES'
 obj.insert_inscritos(post)
